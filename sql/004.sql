c38bac48eb22b3becaac2247d02fe2b62e9df521
lorentz <lorentz.intech@ms188.com>
2025-06-04 04:20:44 +0800

    fffffffff

--- sql/004.sql
+++ sql/004.sql
@@ -22,7 +22,7 @@ SELECT CONCAT('cd /home/www/conversion-api-koa; FFMPEG_BIN="./bin/ffmpeg-app" no
   END
 
 
-	, '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', subtitle, ' -- ', updated_at) 
+	, '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', subtitle, ' -- ', updated_at, ' -- ', ' --https://qzpwa-hw.point-memory.com', pwa_url) 
 FROM `video_movies` 
 WHERE url NOT REGEXP '/aac/h264/hls/' AND 
 url != '' AND 
@@ -55,7 +55,7 @@ ORDER BY  `updated_at`  DESC;
 
 
 
-SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-app" node bin/main.js NonethVideo4Command --app_id=4 --id=', id, ' --url="https://enlvd4cili.ahplm.com', IF(enc_url_265 = '', enc_url, enc_url_265), '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', subtitle, ' -- ', updated_at) 
+SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-app" node bin/main.js NonethVideo4Command --app_id=4 --id=', id, ' --url="https://enlvd4cili.ahplm.com', IF(enc_url_265 = '', enc_url, enc_url_265), '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', subtitle, ' -- ', updated_at, ' --https://qzpwa-hw.point-memory.com', pwa_url) 
 FROM `video_movies` 
 WHERE enc_url NOT REGEXP '/aac/h264/hls/' AND 
 enc_url != '' AND 
