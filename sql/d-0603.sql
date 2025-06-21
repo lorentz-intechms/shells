0b50473411dda68bad861c833ec4b4af2f73d961
lorentz <lorentz.intech@ms188.com>
2025-06-22 07:07:52 +0800

    fffffffff

--- sql/d-0603.sql
+++ sql/d-0603.sql
@@ -5,52 +5,30 @@
 -- WHERE ausa.app_converter_producer_id = 11 AND ausa.file = 1 AND ausa.step = 3;
 
 
--- -- PWA cdn https://qzpwa-hw.point-memory.com
+-- PWA cdn https://qzpwa-hw.point-memory.com
 
--- SELECT CONCAT('cd /home/www/conversion-api-koa; FFMPEG_BIN="./bin/ffmpeg-app" node bin/main.js NonethVideo4Command --app_id=4 --id=', id, ' --url="https://enlvd4cili.ahplm.com', 
---   CASE
---     WHEN LOCATE('/hls/', url) > 0 THEN
---       CONCAT(
---         LEFT(
---           url,
---           LOCATE('/hls/', url) - 1 - 
---             LOCATE('/', REVERSE(LEFT(url, LOCATE('/hls/', url) - 1))) + 1
---         ),
---         SUBSTRING(url, LOCATE('/hls/', url) + LENGTH('/hls/'))
---       )
---     ELSE url
---   END
+SELECT CONCAT('cd /home/www/conversion-api-koa; FFMPEG_BIN="./bin/ffmpeg-app" node bin/main.js NonethVideo4Command --app_id=4 --id=', id, ' --url="https://enlvd4cili.ahplm.com', 
+  CASE
+    WHEN LOCATE('/hls/', url) > 0 THEN
+      CONCAT(
+        LEFT(
+          url,
+          LOCATE('/hls/', url) - 1 - 
+            LOCATE('/', REVERSE(LEFT(url, LOCATE('/hls/', url) - 1))) + 1
+        ),
+        SUBSTRING(url, LOCATE('/hls/', url) + LENGTH('/hls/'))
+      )
+    ELSE url
+  END
 
 
--- 	, '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', subtitle, ' -- ', updated_at, ' -- ', ' --https://qzpwa-hw.point-memory.com', pwa_url) 
--- FROM `video_movies` 
--- WHERE url NOT REGEXP '/aac/h264/hls/' AND 
--- url != '' AND 
--- tencent_enc_url = '' AND
--- id IN 
+	, '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', subtitle, ' -- ', updated_at, ' -- ', ' --https://qzpwa-hw.point-memory.com', pwa_url) 
+FROM `video_movies` 
+WHERE url NOT REGEXP '/aac/h264/hls/' AND 
+url != '' AND 
+tencent_enc_url = '' is_transcoding = 0
 
--- (10043,
--- 10042,
--- 10041,
--- 10040,
--- 10037,
--- 10036,
--- 10035,
--- 10034,
--- 10033,
--- 10032,
--- 10031,
--- 10030,
--- 10029,
--- 10028,
--- 10027,
--- 10026,
--- 10025,
--- 10024,
--- 10023)
---  AND is_transcoding = 0
-
--- ORDER BY  `updated_at`  DESC;
+ORDER BY  `updated_at`  DESC;
 
 
 
---
+++ sql/d-0622.sql
@@ -0,0 +1,22 @@
+SELECT CONCAT('cd /home/www/conversion-api-koa; FFMPEG_BIN="./bin/ffmpeg-app" node bin/main.js NonethVideo4Command --app_id=4 --id=', id, ' --url="https://enlvd4cili.ahplm.com', 
+  CASE
+    WHEN LOCATE('/hls/', url) > 0 THEN
+      CONCAT(
+        LEFT(
+          url,
+          LOCATE('/hls/', url) - 1 - 
+            LOCATE('/', REVERSE(LEFT(url, LOCATE('/hls/', url) - 1))) + 1
+        ),
+        SUBSTRING(url, LOCATE('/hls/', url) + LENGTH('/hls/'))
+      )
+    ELSE url
+  END
+
+
+  , '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', subtitle, ' -- ', updated_at, ' -- ', ' --https://qzpwa-hw.point-memory.com', pwa_url) 
+FROM `video_movies` 
+WHERE url NOT REGEXP '/aac/h264/hls/' AND 
+url != '' AND 
+tencent_enc_url = '' is_transcoding = 0
+
+ORDER BY  `updated_at`  DESC;
