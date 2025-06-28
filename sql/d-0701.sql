85e9b02c0c2724fc841fb1dcd4c45e617fad4bfa
lorentz <lorentz.intech@ms188.com>
2025-06-29 00:59:11 +0800

    fffffffff

--- sql/d-0701.sql
+++ sql/d-0701.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo5Command --app_id=4 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', IF(enc_url_265 = '', enc_url, enc_url_265), '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', ' -- ', updated_at, ' ', pwa_url) 
+SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo5Command --app_id=5 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', IF(enc_url_265 = '', enc_url, enc_url_265), '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', ' -- ', updated_at, ' ', pwa_url) 
 FROM `video_micro_movies` 
 WHERE enc_url NOT REGEXP '/aac/h264/hls/' AND 
 enc_url != '' AND 
@@ -9,7 +9,7 @@ is_transcoding = 1
 
 
 
- SELECT CONCAT('cd /home/www/conversion-api-koa; FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo5Command --app_id=4 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', 
+ SELECT CONCAT('cd /home/www/conversion-api-koa; FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo5Command --app_id=5 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', 
   CASE
     WHEN LOCATE('/hls/', url) > 0 THEN
       CONCAT(
