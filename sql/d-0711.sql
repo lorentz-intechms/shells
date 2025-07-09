70a8615ae2ea47dcddd8e7ee4288d91e036a302f
lorentz <lorentz.intech@ms188.com>
2025-07-09 13:50:49 +0800

    fffffffff

--- sql/d-0711.sql
+++ sql/d-0711.sql
@@ -1,5 +1,5 @@
 SELECT text FROM 
-(SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo5Command --app_id=4 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', IF(enc_url_265 = '', enc_url, enc_url_265), '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # is_transcoding=', is_transcoding, ', -- ', updated_at, ' --https://qzpwa-hw.point-memory.com', pwa_url, ', status=', status) AS text, status
+(SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo5Command --app_id=5 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', IF(enc_url_265 = '', enc_url, enc_url_265), '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # is_transcoding=', is_transcoding, ', -- ', updated_at, ' --https://qzpwa-hw.point-memory.com', pwa_url, ', status=', status) AS text, status
 FROM `video_movies` AS v1
 WHERE enc_url NOT REGEXP '/aac/h264/hls/' AND 
 enc_url != '' AND 
@@ -10,7 +10,7 @@ tencent_enc_url = ''
  UNION
 
 
- SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo5Command --app_id=4 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', 
+ SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo5Command --app_id=5 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', 
   CASE
     WHEN LOCATE('/hls/', url) > 0 THEN
       CONCAT(
