78ec777a25e104d83aaa384b3b113888b4fbae9f
lorentz <lorentz.intech@ms188.com>
2025-07-09 13:52:18 +0800

    fffffffff

--- sql/d-0711.sql
+++ sql/d-0711.sql
@@ -1,6 +1,6 @@
 SELECT text FROM 
 (SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo5Command --app_id=5 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', IF(enc_url_265 = '', enc_url, enc_url_265), '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # is_transcoding=', is_transcoding, ', -- ', updated_at, ' --https://qzpwa-hw.point-memory.com', pwa_url, ', status=', status) AS text, status
-FROM `video_movies` AS v1
+FROM `video_micro_movies` AS v1
 WHERE enc_url NOT REGEXP '/aac/h264/hls/' AND 
 enc_url != '' AND 
 tencent_enc_url = '' 
@@ -26,7 +26,7 @@ tencent_enc_url = ''
 
 
   , '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # is_transcoding=', is_transcoding, ', ' , subtitle, ' -- ', updated_at, ' -- ', pwa_url, ', status=', status) AS text ,status
-FROM `video_movies`  AS v2
+FROM `video_micro_movies`  AS v2
 WHERE url NOT REGEXP '/aac/h264/hls/' AND 
 url != '' AND 
 tencent_enc_url = '' AND is_transcoding = 0 
