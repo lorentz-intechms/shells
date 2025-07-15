7444dffdf0132a55b18c2c9dc5dd4c3856d17229
lorentz <lorentz.intech@ms188.com>
2025-07-15 09:12:36 +0800

    fffffffff

--- sql/d-0712.sql
+++ sql/d-0712.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo31Command --app_id=31 --id=', id, ' --url="https://nl4cgvem.xwzh022.com', IF(enc_url_265 = '', enc_url, enc_url_265), '" --filename="', id, '.mp4" --algorithm="aes-128-ecb" --key="saIZXc4yMvq0Iz56";  # ', ' -- ', updated_at, ' ', pwa_url) 
 FROM `video_content_expands` 
 WHERE enc_url NOT REGEXP '/aac/h264/hls/' AND 
-enc_url != '' 
+enc_url != ''  AND tencent_enc_url = ''
 
 ORDER BY  `id`  ASC;
 
