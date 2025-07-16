06c61b3655ab0d87d2c16c1c16037a86d43ded45
lorentz <lorentz.intech@ms188.com>
2025-07-16 15:37:21 +0800

    fffffffff

--- sql/p-0715.sql
+++ sql/p-0715.sql
@@ -4,3 +4,20 @@ download_path, '" --filename="', id, '.mp4"; # filename=', filename, ' title=', 
 
 FROM `one_content_videos` 
 WHERE type IN ('main', 'preview') ORDER BY id ASC;
+
+
+
+
+
+
+
+SELECT
+CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethM3u8Command --app_id=16 --id=', id, ' --url="https://gjvd-new-obs.obs.ap-southeast-3.myhuaweicloud.com',
+encrypt_path, '" --filename="', id, '.mp4"; # filename=', filename, ' title=', title)
+
+FROM `one_content_videos` 
+WHERE type IN ('main', 'preview') ORDER BY id ASC;
+
+
+
+# https://one-encryption-obs.obs.la-north-2.myhuaweicloud.com
