0ae26d952a45f1c201f923ae2a78ecbddbbfb374
lorentz <lorentz.intech@ms188.com>
2025-08-04 14:48:38 +0800

    f

--- sql/16-0715.sql
+++ sql/16-0715.sql
@@ -1,6 +1,6 @@
 SELECT
 CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo16Command --app_id=16 --id=', id, ' --url="https://vdmk.mfpt8g.com',
-download_path, '" --filename="', id, '.mp4"; # filename=', filename, ' title=', title)
+download_path, '" --filename="', filename, '.mp4"; # filename=', filename, ' title=', title)
 
 FROM `one_content_videos` 
 WHERE type IN ('main', 'preview') ORDER BY id ASC;
