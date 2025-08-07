71e9041b7361cd50115040fbfb52ce3c9f962b9b
lorentz <lorentz.intech@ms188.com>
2025-08-07 08:19:22 +0800

    f

--- sql/16-0715.sql
+++ sql/16-0715.sql
@@ -1,7 +1,8 @@
 SELECT
 CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo16Command --app_id=16 --app_user_name="one_auto_01" ', '--id=', id, ' --url="https://vdmk.mfpt8g.com',
 download_path, '" --filename="', CASE 
-    WHEN LEFT(download_path, 1) = '/' THEN SUBSTRING(download_path, 2)
+    WHEN download_path LIKE '/one/%' THEN SUBSTRING(download_path, 6)
+    WHEN download_path LIKE '/%' THEN SUBSTRING(download_path, 2)
     ELSE download_path
   END, '" ; # filename=', filename, ' title=', title)
 
