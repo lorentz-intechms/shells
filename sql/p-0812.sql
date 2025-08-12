e6020999043de8ce946a1f4a46cc5843a223570c
lorentz <lorentz.intech@ms188.com>
2025-08-12 16:42:20 +0800

    f

--- sql/p-0812.sql
+++ sql/p-0812.sql
@@ -1,6 +1,6 @@
 SELECT
 CONCAT('cd /home/www/conversion-api-koa; sudo FFMPEG_BIN="./bin/ffmpeg-tencent" node bin/main.js NonethVideo16Command --app_id=16 --id=', id, ' --url="https://vdmk.mfpt8g.com',
-download_path, '" --filename="', id, '.mp4"; # filename=', filename, ' title=', title)
+download_path, '" --filename="', filename, '"; # filename=', filename, ' title=', title)
 
 FROM `one_content_videos` 
 WHERE type IN ('main', 'preview') AND filename 
@@ -47777,6 +47777,6 @@ NOT IN (
 "zyhx-sample.mp4"
 )
 
-ORDER BY id ASC;
+ORDER BY id ASC LIMIT 0, 1000;
 
 
