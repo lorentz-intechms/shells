948eefa34a69cfbf9be27508843bec18b2503741
lorentz <lorentz.intech@ms188.com>
2025-11-20 05:58:21 +0800

    1

--- sql/01-0901.sql
+++ sql/01-0901.sql
@@ -30,7 +30,8 @@ WHERE CAST(d1.resolution AS UNSIGNED) = (
 ) AS vd
  ON vv.id = vd.vurl_id
 WHERE vd.path NOT REGEXP '/aac/h264/hls/' AND v.d_hide = 0 AND v.d_status = 1 AND vd.path LIKE '/%' 
-ORDER BY vd.path;
+ORDER BY vd.path 
+LIMIT 0, 1000;
 
 
 
