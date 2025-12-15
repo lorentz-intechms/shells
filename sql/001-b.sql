0908154ce03588dcd42eb375081e7ca527ee3417
lorentz <lorentz.intech@ms188.com>
2025-12-15 10:37:51 +0800

    1

--- sql/001-b.sql
+++ sql/001-b.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT('DELETE FROM mac_vurl_detail WHERE vurl_id =', vurl_id, " AND resolution='480' ;", )
 FROM mac_vurl_detail GROUP BY vurl_id 
 
-HAVING COUNT(*) = 3
+HAVING COUNT(*) >= 3
 ORDER BY vurl_id;
