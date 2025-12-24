e30e7f249ab5920051cdc1702ddbd2626a6caac5
lorentz <lorentz.intech@ms188.com>
2025-12-24 11:16:40 +0800

    1

--- sql/001-b.sql
+++ sql/001-b.sql
@@ -1,5 +1,14 @@
-SELECT CONCAT('DELETE FROM mac_vurl_detail WHERE vurl_id =', vurl_id, " AND resolution='480' ;", )
+SELECT CONCAT('DELETE FROM mac_vurl_detail WHERE vurl_id =', vurl_id, " AND resolution='480' ;")
 FROM mac_vurl_detail GROUP BY vurl_id 
 
 HAVING COUNT(*) >= 3
 ORDER BY vurl_id;
+
+
+全量切片那边开到满，一个月平均下来
+瓜子：一台可以处理 630 视频，75GB 流量，6000 分钟时常
+果酱：一台可以处理 290 视频，170GB 流量，1900 分钟时常
+
+全量这边基本上就是开到满，线性去转码
+增量那边不是，增量的运营至少 一天24小时中尖峰的 4小时，上传今天 80% 的量。
+增量那边无法完全保证 运营上传了 就系统有空间给他用，尤其是 周五晚上 瓜子 果酱 都在上传的时候
---
+++ sql/011.sql
@@ -0,0 +1,41 @@
+## q2wjmspnn.ybdxjy.com   qqc 未加密视频域名
+## f68c.fa135ka.com   qqc 加密视频域名
+
+
+SELECT 
+    m.id AS video_id,
+    m.video_name AS video_name,
+    CONCAT(TRIM(TRAILING '/' FROM cdn_d.domain), '/', TRIM(LEADING '/' FROM d.path)) AS decryption_url,
+    CONCAT(TRIM(TRAILING '/' FROM cdn_e.domain), '/', TRIM(LEADING '/' FROM e.path)) AS encryption_url
+FROM video_movies m
+INNER JOIN video_movie_urls d ON d.movie_id = m.id 
+    AND d.type_file = 'DECRYPTION' 
+    AND d.type_extension = '.m3u8'
+INNER JOIN video_movie_urls e ON e.movie_id = m.id 
+    AND e.type_file = 'ENCRYPTION' 
+    AND e.type_extension = '.m3u8'
+-- 未加密CDN
+INNER JOIN (
+    SELECT number, domain
+    FROM video_cdns 
+    WHERE type = '0' 
+        AND is_encrypt = 0 
+        AND status = '0'
+        AND app_id IN (0, 3)
+    ORDER BY app_id DESC, balance_weight DESC
+) cdn_d ON cdn_d.number = m.cdn_number
+-- 加密CDN
+INNER JOIN (
+    SELECT number, domain
+    FROM video_cdns 
+    WHERE type = '0' 
+        AND is_encrypt = 1 
+        AND status = '0'
+        AND app_id IN (0, 3)
+    ORDER BY app_id DESC, balance_weight DESC
+) cdn_e ON cdn_e.number = m.cdn_number
+WHERE m.status = 1
+    AND m.qz_video_id = 0
+    AND d.path IS NOT NULL AND d.path != ''
+    AND e.path IS NOT NULL AND e.path != ''
+GROUP BY m.id;
